0f518e1880ecd30ba650fab256b5137c61dfb2a3
AstRatJP <takuyanadec@docomo.ne.jp>
2026-01-04 18:50:22 +0900

    Initial commit

---
+++ svgo.mjs
@@ -0,0 +1,54 @@
+export default {
+	multipass: true,
+	js2svg: {
+		indent: '\t',
+		pretty: true,
+	},
+	plugins: [
+		"convertStyleToAttrs",
+		"removeDimensions",
+		"removeXMLNS",
+		"removeXlink",
+		"removeUnusedNS",
+		
+		"removeDoctype",
+		"removeXMLProcInst",
+		"removeComments",
+		"removeMetadata",
+		"removeEditorsNSData",
+		"cleanupAttrs",
+		//"mergeStyles",
+		//"inlineStyles",
+		//"minifyStyles",
+		"cleanupIds",
+		"removeUselessDefs",
+		"cleanupNumericValues",
+		"convertColors",
+		"removeUnknownsAndDefaults",
+		"removeNonInheritableGroupAttrs",
+		"removeUselessStrokeAndFill",
+		"cleanupEnableBackground",
+		"removeHiddenElems",
+		"removeEmptyText",
+		//"convertShapeToPath",
+		"convertEllipseToCircle",
+		"moveElemsAttrsToGroup",
+		//"moveGroupAttrsToElems",
+		"collapseGroups",
+		"convertPathData",
+		"convertTransform",
+		"removeEmptyAttrs",
+		"removeEmptyContainers",
+		//"mergePaths",
+		"sortAttrs",
+		"sortDefsChildren",
+		"removeDesc",
+		
+		{
+			name: "removeAttrs",
+			params: {
+				attrs: ["path:color", "*:style"]
+			}
+		}
+	]
+}
